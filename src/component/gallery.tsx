9c0845920faab43b0fb9f0d3840f516fa5130cc9
ABIARASYDRAJATPRAKOSO <abiarasy096@gmail.com>
2024-04-17 17:11:23 +0700

    Jawaban Soal Praktikum 4

--- src/component/gallery.tsx
+++ src/component/gallery.tsx
@@ -5,17 +5,26 @@ export default function Gallery(){
 
     function handleClick(){
         setIndex(index+1);
+        if (index==4){
+            setIndex(0)
+        }
     }
 
+    function handleClick2(){
+        setIndex(index-1);
+        if (index==0){
+            setIndex(4)
+        }
+    }
     let sculpture  = sculptureList[index];
 
 return(
     <>
     <button 
-    onClick={handleClick}
-    className="bg-blue-500 hover:bg-blue-700 p-2 m-2 rounded">Artikel Selanjutnya</button>
-    <h2><i>{sculpture.name}</i>oleh {sculpture.artist}</h2>
-    <h3>({index+ 1} dari {sculptureList.length})</h3>
+    onClick={handleClick2}
+    className="bg-blue-500 hover:bg-blue-700 p-2 m-2 rounded">Artikel Sebelumnya</button>
+    <h2><i>{sculpture.name}</i> oleh {sculpture.artist}</h2>
+    <h3>({index+1} dari {sculptureList.length})</h3>
     <img src={sculpture.url} alt={sculpture.alt}/>
     <p>
         {sculpture.description}
